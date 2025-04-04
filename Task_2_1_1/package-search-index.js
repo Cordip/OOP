1d43124cd7c977835e5645c91189169e6e5d401f
Cordip <Cordip@users.noreply.github.com>
2025-04-04 08:08:02 +0000

    Deploying to gh-pages from @ Cordip/OOP@c8cb7fd9e1c5a7e364e46d01e729c4adacf646a4 🚀

--- Task_2_1_1/package-search-index.js
+++ Task_2_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.simplenumbers"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.primes.checker"}];updateSearchResults();
